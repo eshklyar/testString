883f99c1588f4135d06d33dc58c5318bd8181c01
eshklyar <eshklyar@gmail.com>
2014-12-08 19:57:40 -0600

    test

--- testString/ViewController.m
+++ testString/ViewController.m
@@ -16,12 +16,113 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *abc = @"this is test";
+
+    abc = [NSString stringWithFormat:@"%@%@",[[abc substringToIndex:1] uppercaseString],[abc substringFromIndex:1] ];
+    NSLog(@"abc = %@",abc);
+
+
+    NSString *make = @"Porsche";
+    NSString *model = @"911";
+    int year = 1968;
+    NSString *message = [NSString stringWithFormat:@"That's a %@ %@ from %d!",
+                         make, model, year];
+    NSString* myMessage = [self replaceSpaces:message];
+    NSLog(@"message is %@", message);
+   NSLog(@"myMessage is %@", myMessage);
+
+    NSString* myString = [self concatString:make withString:model];
+     NSLog(@"myString is %@", myString);
+    NSRange range = [message rangeOfString:make];
+    NSInteger myStart = range.location;
+    NSInteger myLength = range.length;
+    NSLog(@"start is %lu", (unsigned long)myStart);
+    NSLog(@"lengh is %lu", (unsigned long)myLength);
+    NSLog(@"haha %lu", (unsigned long)[make length]);
+
+    char* c = [model UTF8String];
+    NSLog(@"char is %s", c);
+    NSLog(@"char is %c", c[0]);
+    NSLog(@"char 2 is %c", [model characterAtIndex:0]);
+
+
+
+    NSString* revString = [self reversingString:make] ;
+//    NSString *txt = @"hi my friends!";
+//    revString = [revString stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[revString substringToIndex:1] uppercaseString]];
+    NSLog(@"rev %@", revString);
+
+    NSString *anotherString = @"abccc";
+    NSRange anotherRange = [anotherString rangeOfString:@"c" options:NSCaseInsensitiveSearch range:NSMakeRange(0,anotherString.length)];
+
+    NSLog(@"anotherRange %lu", (unsigned long)anotherRange.location);
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSString*) concatString: (NSString*) firstString withString: (NSString*) secondString{
+
+    NSString* myString = [NSString stringWithFormat:@"%@ %@", firstString, secondString];
+    return myString;
+}
+// reverse string and changing the case
+-(NSString*) reversingString:(NSString *)someString
+{
+
+    NSMutableString *reverseString = [[NSMutableString alloc] initWithCapacity:[someString length]];
+    for(NSInteger i=[someString length] -1;i>=0;i--)
+    {
+        [reverseString appendString:[NSString stringWithFormat:@"%c",[someString characterAtIndex:i]]];
+    }
+
+
+//    unichar char = [codeString characterAtIndex:0];
+//    unichar char = [codeString substringToIndex:1]
+//    NSCharacterSet *letters = [NSCharacterSet letterCharacterSet];
+//    if ([letters characterIsMember:char]) {
+//        // The first character is a letter in some alphabet
+//    }
+
+
+    if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[someString characterAtIndex:0]]) {
+
+        NSString* reverseStringLowerCase = [reverseString lowercaseString];
+        NSLog(@"lowercase %@", reverseStringLowerCase);
+
+        NSString* reverseStringUpperCase = [reverseStringLowerCase stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[reverseStringLowerCase substringToIndex:1] uppercaseString]];
+        NSLog(@"uppercase %@", reverseStringUpperCase);
+    }
+
+    return reverseString;
 }
 
+-(NSString*)replaceSpaces: (NSString*) someString {
+
+    someString = [someString stringByReplacingOccurrencesOfString:@" " withString:@""];
+
+    return someString;
+}
+
+-(void)anotherReverse:(NSString*)someString{
+
+    NSMutableString *reversedString = [NSMutableString string];
+    NSInteger charIndex = [someString length];
+        while (charIndex > 0) {
+        charIndex--;
+        NSRange subStrRange = NSMakeRange(charIndex, 1);
+        [reversedString appendString:[someString substringWithRange:subStrRange]];
+        }
+    NSLog(@"%@", reversedString);
+    }
+-(void)yetAnotherReverse:(NSString*)someString{
+
+
+    NSMutableString *reversedString = [NSMutableString stringWithCapacity:[someString length]];
+
+    [someString enumerateSubstringsInRange:NSMakeRange(0,[someString length])
+                                 options:(NSStringEnumerationReverse | NSStringEnumerationByComposedCharacterSequences)
+                              usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+                                  [reversedString appendString:substring];
+                              }];
+}
 @end
